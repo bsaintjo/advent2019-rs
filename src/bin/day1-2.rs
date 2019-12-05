d10d2200f016c59b473a2fe4ee3cd636c6314e3d
Brandon Saint-John <bsaintjo@ucsc.edu>
2019-12-05 01:54:28 -0800

    Initial commit, day one complete

---
+++ src/bin/day1-2.rs
@@ -0,0 +1,19 @@
+use std::io::{self, BufRead};
+use std::cmp;
+
+
+fn main() {
+    let stdin = io::stdin();
+    let mut iter = stdin.lock().lines();
+    let mut total_fuel = 0;
+    while let Some(Ok(line)) =  iter.next() {
+        let mut mass = line.parse::<i32>().unwrap();
+        while mass > 0 {
+            let fuel = ((mass as f32 / 3.0).floor() - 2.0) as i32;
+            let fuel = cmp::max(fuel, 0);
+            total_fuel += fuel;
+            mass = fuel;
+            println!("{}", mass);
+    }}
+    println!("{}", total_fuel);
+}
